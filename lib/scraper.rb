bcba8aaf98a52c8f60fb7538b2ded56e2ea07270
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-04-09 14:56:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,6 +27,12 @@ class Scraper
 #Responsible for actually instantiating Course objects and giving
 #Each course object the correct title, schedule and description attribute that we scraped from the page.
   def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
   end
 
 #Calls on .make_courses and then iterates over all of the
