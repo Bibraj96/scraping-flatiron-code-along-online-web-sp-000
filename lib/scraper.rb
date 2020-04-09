15d16b941626f4ff7b224cc93a7c28ea3d2bc61a
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-04-09 14:58:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,6 @@ require 'pry'
 
 require_relative './course.rb'
 
-class Scraper
-
 #Responsible for using Nokogiri and open-uri to grab the entire HTML document from the web page.
 
 #Calls on .make_courses and then iterates over all of the
@@ -17,26 +15,16 @@ class Scraper
 #Responsible for using a CSS selector to grab all of the HTML elements that contain a course.
 #The return value of this method should be a collection of Nokogiri XML elements, each of which describes a course offering.
 
+class Scraper
 
   def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-
-    #doc.css(".post").each do |post|
-    #  course = Course.new
-    #  course.title = post.css("h2").text
-    #  course.schedule = post.css(".date").text
-    #  course.description = post.css("p").text
-    #end
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
-#Responsible for using a CSS selector to grab all of the HTML elements that contain a course.
-#The return value of this method should be a collection of Nokogiri XML elements, each of which describes a course offering.
   def get_courses
     self.get_page.css(".post")
   end
 
-#Responsible for actually instantiating Course objects and giving
-#Each course object the correct title, schedule and description attribute that we scraped from the page.
   def make_courses
     self.get_courses.each do |post|
       course = Course.new
@@ -46,11 +34,8 @@ class Scraper
     end
   end
 
-#Calls on .make_courses and then iterates over all of the
-#Courses that get created to puts out a list of course offerings.
   def print_courses
     self.make_courses
-      self.make_courses
     Course.all.each do |course|
       if course.title && course.title != ""
         puts "Title: #{course.title}"
@@ -62,4 +47,4 @@ class Scraper
 
 end
 
-Scraper.new.get_page
+Scraper.new.print_courses
